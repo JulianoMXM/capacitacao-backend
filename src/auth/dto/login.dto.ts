a32f99d355ca117a5a8c916f3bf8fa2111da4462
JulianoMXM <juliano.massocalegal@gmail.com>
2026-01-31 19:59:35 -0300

    feat: Adiciona validação nos dados de autenticação.

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class LoginDto {
+    @IsEmail({}, {message: 'Email Inválido.'})
     email: string;
+
+    @IsString({message: 'Senha inválida'})
+    @IsNotEmpty({message: 'A senha não pode ser vazia.'})
     password: string;
 }
