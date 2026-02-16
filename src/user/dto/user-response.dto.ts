b2e336edc34ab09d544940b3f2ad3c67440e2329
JulianoMXM <juliano.massocalegal@gmail.com>
2026-02-16 17:14:27 -0300

    style: Ajuste automático do lint

--- src/user/dto/user-response.dto.ts
+++ src/user/dto/user-response.dto.ts
@@ -1,13 +1,13 @@
-import { Exclude, Expose } from "class-transformer";
+import { Exclude, Expose } from 'class-transformer';
 
 @Exclude()
-export class UserResponseDto{
-    @Expose()
-    id: string;
+export class UserResponseDto {
+  @Expose()
+  id: string;
 
-    @Expose()
-    name: string;
+  @Expose()
+  name: string;
 
-    @Expose()
-    email: string;
+  @Expose()
+  email: string;
 }
