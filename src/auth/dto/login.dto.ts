888e94ea8eab21ab04ed22f2930505f3f1c67372
JulianoMXM <juliano.massocalegal@gmail.com>
2026-02-16 15:52:51 -0300

    style: Ajuste automático de formatação

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
-    @IsEmail({}, {message: 'Email Inválido.'})
-    email: string;
+  @IsEmail({}, { message: 'Email Inválido.' })
+  email: string;
 
-    @IsString({message: 'Senha inválida'})
-    @IsNotEmpty({message: 'A senha não pode ser vazia.'})
-    password: string;
+  @IsString({ message: 'Senha inválida' })
+  @IsNotEmpty({ message: 'A senha não pode ser vazia.' })
+  password: string;
 }
