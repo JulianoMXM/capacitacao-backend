18169d5e63801cf00dc5c35c3cfc2a5b82966990
JulianoMXM <juliano.massocalegal@gmail.com>
2026-02-19 16:49:37 -0300

    refact: Ajusta o LoginDto para garantir que o email não é vazio.

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,9 +2,10 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
   @IsEmail({}, { message: 'Email Inválido.' })
+  @IsNotEmpty({ message: 'Email é obrigatório.' })
   email: string;
 
   @IsString({ message: 'Senha inválida' })
-  @IsNotEmpty({ message: 'A senha não pode ser vazia.' })
+  @IsNotEmpty({ message: 'A senha é obrigatória.' })
   password: string;
 }
