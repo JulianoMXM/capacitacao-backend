e49c5dff6da90cb40fdbf3fe1d5a958fcbd4f3f9
JulianoMXM <juliano.massocalegal@gmail.com>
2026-02-19 16:51:09 -0300

    feat: Importa UserModule, PassportModule e JwtModule, utilizando ou o env ou uma chave genérica, além disso, adiciona LocalStrategy e JwtStrategy nos providers.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from 'src/strategies/local-strategy';
+import { JwtStrategy } from 'src/strategies/jwt-strategy';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'JWT_SECRET',
+      signOptions: { expiresIn: '3h' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [],
 })
 export class AuthModule {}
