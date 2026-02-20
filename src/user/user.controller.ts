1d7e17f013cd0ece262d51e02f08d24bd40fa630
JulianoMXM <juliano.massocalegal@gmail.com>
2026-02-20 17:30:10 -0300

    feat: Adiciona Guard nas rotas que de usuário que serão utilizadas

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,10 +6,12 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth-guard';
 
 @Controller('user')
 export class UserController {
@@ -20,23 +22,27 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
-  @Get()
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
   findAll() {
     return this.userService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @Patch('me')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @UseGuards(JwtAuthGuard)
+  @Patch('me/password')
+  updatePassword(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('me')
+  delete(@Param('id') id: string) {
     return this.userService.remove(+id);
   }
 }
