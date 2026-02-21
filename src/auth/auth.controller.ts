fbf336852286274e0ba71dab76295fd8a43cc5c4
JulianoMXM <juliano.massocalegal@gmail.com>
2026-02-21 15:12:03 -0300

    refact: Altera LoginRequest para RequestWithUser

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { LocalAuthGuard } from './guards/auth-guard';
-import type { LoginRequest } from './types/request-with-user';
+import type { RequestWithUser } from './types/request-with-user';
 
 @Controller('auth')
 export class AuthController {
@@ -11,7 +11,7 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  login(@Request() req: LoginRequest, @Body() loginDto: LoginDto) {
+  login(@Request() req: RequestWithUser, @Body() loginDto: LoginDto) {
     return this.authService.login(req.user);
   }
 }
