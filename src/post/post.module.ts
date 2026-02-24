064ee72ed2dca73f2faebd0cbd3cdac61c57d285
JulianoMXM <juliano.massocalegal@gmail.com>
2026-02-24 20:47:53 -0300

    feat: Faz os Imports com a espera do AuthModule para não gerar erros na validação do token jwt

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post]), forwardRef(() => AuthModule)],
   controllers: [PostController],
   providers: [PostService],
 })
