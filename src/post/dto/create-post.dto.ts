7e6c46f71b012750d9c3aee93448296c09e47bed
JulianoMXM <juliano.massocalegal@gmail.com>
2026-02-24 20:46:38 -0300

    feat: Cria a dto de criação de post

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Título é obrigatório.' })
+  title: string
+
+  @IsString()
+  @IsNotEmpty({ message: 'Conteúdo é obrigatório.' })
+  @MinLength(50, { message: 'Conteúdo deve conter no mínimo 50 caracteres.'})
+  content: string
+}
