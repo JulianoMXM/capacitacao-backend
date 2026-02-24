b4273460c67b3c4332fc42782820a39fa85b9b48
JulianoMXM <juliano.massocalegal@gmail.com>
2026-02-24 20:47:09 -0300

    feat: Adiciona as rotas utilizadas com o guard de jwt e chamado do service

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,34 +1,65 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, Query } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth-guard';
+import type { RequestWithUser } from 'src/auth/types/request-with-user';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
-  @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postService.create(createPostDto);
+  @UseGuards(JwtAuthGuard)
+  @Post('me')
+  create(@Body() createPostDto: CreatePostDto, @Request() req: RequestWithUser) {
+    return this.postService.create(createPostDto, req.user.id);
   }
 
   @Get()
-  findAll() {
-    return this.postService.findAll();
+  getAll(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '10'
+  ) {
+    return this.postService.findAll(Number(page), Number(limit));
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getAllMe(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '10',
+    @Request() req: RequestWithUser
+  ) {
+    return this.postService.findAllFromUser(req.user.id, Number(page), Number(limit));
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postService.update(+id, updatePostDto);
+  @Get(':slug')
+  getOne(@Param('slug') slug: string) {
+    return this.postService.findOne(slug);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postService.remove(+id);
+  @UseGuards(JwtAuthGuard)
+  @Get('me/:slug')
+  getOneMe(
+    @Param('slug') slug: string,
+    @Request() req: RequestWithUser) {
+    return this.postService.findOneFromUser(req.user.id, slug);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('me/:id')
+  update(
+    @Param('id') postId: string, 
+    @Body() updatePostDto: UpdatePostDto,
+    @Request() req: RequestWithUser) {
+    return this.postService.update(req.user.id, postId, updatePostDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('me/:id')
+  remove(
+    @Param('id') id: string,
+    @Request() req: RequestWithUser) {
+    return this.postService.remove(req.user.id, id); 
   }
 }
