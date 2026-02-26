0dd5ac27c8e78998363ed3eba6f899f5df76679e
JulianoMXM <juliano.massocalegal@gmail.com>
2026-02-25 22:31:56 -0300

    feat: Cria o módulo de comentários com as importações necessárias

---
+++ src/comment/comment.module.ts
@@ -0,0 +1,14 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { CommentController } from './comment.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comment } from './entities/comment.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { Post } from 'src/post/entities/post.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Comment, Post]), forwardRef(() => AuthModule)],
+  controllers: [CommentController],
+  providers: [CommentService],
+})
+export class CommentModule {}
